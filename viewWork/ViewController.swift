8791c7addcdb8e4665623b1c7bebceef5a598af8
Marat <kosjachok8@yandex.ru>
2022-05-31 16:13:08 +0300

    Initial Commit

---
+++ viewWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewWork
+//
+//  Created by Marat_FMK on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
